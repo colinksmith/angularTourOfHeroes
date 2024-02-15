dda1be41ebc85b327bce570399e031a9447e2797
Colin Smith <colin.kenjiro.smith@gmail.com>
2024-02-15 13:20:36 -0500

    copied given code end step 2

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
+import {AppComponent} from './app/app.component';
 
+bootstrapApplication(AppComponent, {
+  providers: [provideProtractorTestingSupport()],
+});
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
